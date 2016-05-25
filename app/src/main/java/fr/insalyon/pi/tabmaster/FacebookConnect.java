b8c0ef1bd0f9fcd197e55fd8472cb18f4810bbc5
Nicolas <nicolas.minghetti@gmail.com>
2016-05-25 23:33:42 +0200

    Récupération infos utilisateur

--- app/src/main/java/fr/insalyon/pi/tabmaster/FacebookConnect.java
+++ app/src/main/java/fr/insalyon/pi/tabmaster/FacebookConnect.java
@@ -3,12 +3,15 @@ package fr.insalyon.pi.tabmaster;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -29,6 +32,12 @@ public class FacebookConnect extends Activity{
 
         setContentView(R.layout.facebook_connection);
         info = (TextView)findViewById(R.id.info);
+
+        boolean loggedIn = AccessToken.getCurrentAccessToken() != null;
+        Log.i("Connection State:", String.valueOf(loggedIn));
+        if(loggedIn){
+            Log.i("Profile info", Profile.getCurrentProfile().getLastName());
+        }
         loginButton = (LoginButton)findViewById(R.id.login_button);
         //source : http://code.tutsplus.com/tutorials/quick-tip-add-facebook-login-to-your-android-app--cms-23837
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
