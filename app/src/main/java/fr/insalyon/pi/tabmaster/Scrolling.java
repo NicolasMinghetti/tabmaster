04d5a9890694493dde4b4c1bdf86467049642e27
Nicolas <nicolas.minghetti@gmail.com>
2016-05-30 11:55:32 +0200

    Ajouté layout scrolling2

--- app/src/main/java/fr/insalyon/pi/tabmaster/Scrolling.java
+++ app/src/main/java/fr/insalyon/pi/tabmaster/Scrolling.java
@@ -14,7 +14,7 @@ public class Scrolling extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.scrolling_activity);
+        setContentView(R.layout.scrolling_activity2);
 
 
         final ImageView playButton=(ImageView) findViewById(R.id.playButton);
