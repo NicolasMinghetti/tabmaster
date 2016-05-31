64fc6232c5ad9a0e96fd39e6652dca015063e4e6
Nicolas <nicolas.minghetti@gmail.com>
2016-05-31 11:48:43 +0200

    Ajouté app Snippet

--- backend/tabmaster/settings.py
+++ backend/tabmaster/settings.py
@@ -38,12 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'snippets.apps.SnippetsConfig',
 ]
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 10
-}
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
