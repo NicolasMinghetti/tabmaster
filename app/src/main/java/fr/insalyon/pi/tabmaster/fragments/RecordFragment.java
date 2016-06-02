812608ed4d211e1b898ece2c4c3bafa415d70930
Ugo Aussourd <ugo.aussourd@gmail.com>
2016-06-02 18:17:48 +0200

    Implements sound recording and playing

--- app/src/main/java/fr/insalyon/pi/tabmaster/fragments/RecordFragment.java
+++ app/src/main/java/fr/insalyon/pi/tabmaster/fragments/RecordFragment.java
@@ -1,21 +1,27 @@
 package fr.insalyon.pi.tabmaster.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import fr.insalyon.pi.tabmaster.R;
+import fr.insalyon.pi.tabmaster.RecordTestActivity;
 
 /**
  * Created by Ugo on 31/05/2016.
  */
 public class RecordFragment extends android.support.v4.app.Fragment {
 
-    TextView title;
-    Context ctx;
+    private TextView title;
+    private Context ctx;
+    private Button recTestBtn;
+    private Button recBtn;
+    private Button playBtn;
 
     public static RecordFragment newInstance() {
         RecordFragment newFragment = new RecordFragment();
@@ -28,6 +34,7 @@ public class RecordFragment extends android.support.v4.app.Fragment {
 
         //Instancing UI elements
         title = (TextView)view.findViewById(R.id.rec_title);
+        recTestBtn = (Button)view.findViewById(R.id.rec_test_button);
         return view;
     }
 
@@ -35,6 +42,15 @@ public class RecordFragment extends android.support.v4.app.Fragment {
         super.onActivityCreated(savedInstanceState);
         //Get activity that uses this fragment
         ctx = getActivity();
+        recTestBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent nextActivity = new Intent(ctx, RecordTestActivity.class);
+                startActivity(nextActivity);
+            }
+        });
     }
 
 }
