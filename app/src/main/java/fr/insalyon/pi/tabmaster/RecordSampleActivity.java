74b004ef9bed6bbfc7006749be9b70dcf183daad
Ugo Aussourd <ugo.aussourd@gmail.com>
2016-06-09 14:36:36 +0200

    Implements sample sending to server via HTTP

--- app/src/main/java/fr/insalyon/pi/tabmaster/RecordSampleActivity.java
+++ app/src/main/java/fr/insalyon/pi/tabmaster/RecordSampleActivity.java
@@ -1,5 +1,6 @@
 package fr.insalyon.pi.tabmaster;
 
+import android.content.Context;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
@@ -10,11 +11,23 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
-/**
- * Created by Ugo on 06/06/2016.
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+
+/* Created by Ugo on 06/06/2016.
  */
 public class RecordSampleActivity extends AppCompatActivity {
     private AudioIn ai;
+    Context ctx;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,20 +52,22 @@ public class RecordSampleActivity extends AppCompatActivity {
         enableButton(R.id.btnStop, isRecording);
     }
 
-    private void startRecording() {
-        ai = new AudioIn();
+    private void startRecording() throws IOException {
+        ai = new AudioIn(this);
     }
 
-    private void stopRecording() {
-        ai.close();
-    }
+    private void stopRecording() {ai.close();}
 
     private View.OnClickListener btnClick = new View.OnClickListener() {
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.btnStart: {
                     enableButtons(true);
-                    startRecording();
+                    try {
+                        startRecording();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 }
                 case R.id.btnStop: {
@@ -71,63 +86,103 @@ public class RecordSampleActivity extends AppCompatActivity {
         }
         return super.onKeyDown(keyCode, event);
     }
-}
-
-class AudioIn extends Thread {
-    private boolean stopped    = false;
-    private int max = 0;
-
-    AudioIn() {
-
-        start();
-    }
-
-    @Override
-    public void run() {
-        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
-        AudioRecord recorder = null;
-        short[]   buffer  = new short[44100];
-
-        try { // ... initialise
-
-            int N = AudioRecord.getMinBufferSize(44100,AudioFormat.CHANNEL_IN_MONO,AudioFormat.ENCODING_PCM_16BIT);
-
-            recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
-                    44100,
-                    AudioFormat.CHANNEL_IN_MONO,
-                    AudioFormat.ENCODING_PCM_16BIT,
-                    N*10);
-
-            recorder.setPositionNotificationPeriod(44100);
-
-            recorder.startRecording();
-
-            // ... loop
 
 
 
-            while(!stopped) {
-                N = recorder.read(buffer,0,buffer.length);
-                process(buffer);
+    class AudioIn extends Thread {
+        private boolean stopped = false;
+        Context act;
+        RequestQueue queue;
+        final String url = "http://10.43.2.151:80/recup/";
+
+
+        AudioIn(AppCompatActivity parent) {
+            start();
+            act = parent;
+        }
+
+        @Override
+        public void run() {
+            android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
+
+            AudioRecord recorder = null;
+            short[] buffer = new short[44100];
+            queue = Volley.newRequestQueue(act);
+
+            try { // ... initialise
+
+                //set audio
+                int N = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+
+                recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                        44100,
+                        AudioFormat.CHANNEL_IN_MONO,
+                        AudioFormat.ENCODING_PCM_16BIT,
+                        N * 10);
+
+                recorder.setPositionNotificationPeriod(44100);
+                recorder.startRecording();
+
+                //set connection
+                URL urlObj = new URL(url);
+                HttpURLConnection urlCon = (HttpURLConnection) urlObj.openConnection();
+                //add request header
+                urlCon.setRequestMethod("POST");
+
+
+                // ... loop
+                while (!stopped) {
+                    N = recorder.read(buffer, 0, buffer.length);
+                    process(buffer, urlCon);
+                }
+
+                recorder.stop();
+                recorder.release();
+            } catch (Throwable x) {
+                Log.w("AudioIn", "Error reading voice audio", x);
+            } finally {
+                close();
+            }
+        }
+
+        private void process(short[] buffer, HttpURLConnection urlCon) throws AuthFailureError {
+            String dataToSend = "";
+            dataToSend = Arrays.toString(buffer);
+            //dataToSend = dataToSend.replaceAll(" ", "").replace("[", "").replace("]", "");
+            System.out.println(Arrays.toString(buffer));
+
+            try{
+
+                // Send post request
+                byte[] outputInBytes = dataToSend.getBytes("UTF-8");
+                OutputStream os = urlCon.getOutputStream();
+                os.write( outputInBytes );
+                os.close();
+
+                int responseCode = urlCon.getResponseCode();
+                System.out.println("Response Code : " + responseCode);
+
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(urlCon.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                //print result
+                System.out.println(response.toString());
+            }catch(Exception e){
             }
 
-            recorder.stop();
-            recorder.release();
-        } catch(Throwable x) {
-            Log.w("AudioIn","Error reading voice audio",x);
-        } finally {
-            close();
+
         }
-    }
 
-    private void process(short[] buffer){
-        for(int i =0; i<buffer.length;i++){
-            //  Log.w("AudioIn", "Smpl[" + i +"] = " + buffer[i]);
+        void close() {
+            stopped = true;
         }
-    }
 
-    void close() {
-        stopped = true;
     }
-
 }
