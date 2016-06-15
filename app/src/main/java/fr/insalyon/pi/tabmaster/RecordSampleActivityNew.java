35aef425859236a0e0c636cb8364a95e761f8d3f
Nicolas <nicolas.minghetti@gmail.com>
2016-06-15 17:58:32 +0200

    Ajouté fonctionnalité scrolling en continu

--- app/src/main/java/fr/insalyon/pi/tabmaster/RecordSampleActivityNew.java
+++ app/src/main/java/fr/insalyon/pi/tabmaster/RecordSampleActivityNew.java
@@ -360,11 +360,10 @@ public class RecordSampleActivityNew extends AppCompatActivity {
                             textView = new TextView(getApplicationContext());
                             //textView.setText(s.charAt(0) + "\n" + s.charAt(1) + "\n" + s.charAt(2) + "\n" + s.charAt(3) + "\n" + s.charAt(4) + "\n" + s.charAt(5) + "\n");
                             textView.setText("1\n2\n3\n4\n5\n5\n");
-                            textView.setTextSize(30);
+                            textView.setTextSize(100);
                             textView.setTypeface(Typeface.MONOSPACE);
                             parentView.addView(textView);
                             bpmLabel.setText("test");
-
                         }
                     });
                     System.out.println("textview a été incrémenté");
